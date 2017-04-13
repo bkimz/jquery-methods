7d2a89649c4560069ed70af1a570b125bbebffe7
bkimz <writetobriankim@gmail.com>
2017-04-13 15:18:16 -0400

    first commit

---
+++ jquerymethods.js
@@ -0,0 +1,29 @@
+//jquery methods
+/*
+select then get/set
+$("div").text() ---> gets all divs text
+$("div").text("hello world") ---> sets all divs text to "hello world"
+
+$("p").html() ---> gets all paragraph html
+$("p").html("<p>All new content. <em>You bet!</em></p>") ---> replaces all paragraphs with new html.
+Similar to DOM inner HTML.
+
+$(".classInput").value() ---> retrieve values from classinput.
+$("#yourInput").value("") ---> sets the value for yourInput to be blank.
+
+$("img").attr() --> gets all the attributes of images.
+$("img").attr("title", "A stormy night") --> sets the title attribute to "A stormy night"
+
+$("cssSelector").css("color", "pink") --> modifies the css of all cssSelector objects so that the color is pink. 
+
+text()
+html()
+attr()
+val()
+addClass()
+removeClass()
+toggleClass()
+
+
+
+*/
